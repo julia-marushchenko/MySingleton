5f896f83e19995654e434a51ac07f03c76ba9ef6
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-03-13 05:42:15 +0100

    Add files via upload
    Committing Singleton class.

---
+++ src/com/mypatterns/Singleton.java
@@ -0,0 +1,44 @@
+/**
+ *  Java program to demonstrate Singleton pattern.
+ */
+package com.mypatterns;
+
+/**
+ *  Singleton class.
+ */
+public class Singleton extends Thread {
+
+    // Creating instance of Singleton class.
+    private  static Singleton instanse = null;
+
+    // Method to initialize Singleton instance.
+    public static Singleton createInstance () throws InterruptedException {
+        instanse = new Singleton();
+
+        // Printing that class object created.
+        System.out.println("Singleton is started.");
+
+        // Singleton is alive 34567 milliseconds.
+        instanse.wait(34567L);
+
+        // Return statement.
+        return instanse;
+
+    }
+
+
+    // Entry point for JVM.
+    public static void main(String[] args) throws InterruptedException {
+
+        try {
+
+            // Starting Singleton application.
+            Runtime.getRuntime().addShutdownHook(Singleton.createInstance());
+
+        } catch (IllegalMonitorStateException ex) {
+
+            // Printing hash of exception.
+            System.out.println(ex.getStackTrace());
+        }
+    }
+}
